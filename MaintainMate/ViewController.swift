f195c9543380f9e87333e5e1b2b1ae6bde3fd42c
ssuryaw21 <ssuryaw2@uncc.edu>
2024-02-12 14:54:57 -0500

    Initial Commit
    Signed-off-by: ssuryaw21 <ssuryaw2@uncc.edu>

---
+++ MaintainMate/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MaintainMate
+//
+//  Created by Shruti Suryawanshi on 2/12/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
